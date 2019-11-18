13152a8a25411beee38c5e32a59091bd58ab86ba
jrb <jrb.youssef@gmail.com>
2019-11-18 08:24:31 +0100

    setup client

--- server.js
+++ server.js
@@ -3,41 +3,57 @@
  */
 const express = require("express");
 const app = express();
-const port = 3000;
 // http need to create the server
 const http = require("http").createServer();
 // we need to pass the http to the io()
 const io = require("socket.io")(http);
 
-// When a user connect to the server
-// io.on("connection", socket => {
-//	 // we emmit an event named 'welcome'
-//	 socket.emit("welcome", "Hello and Welcome to SOCKET.io server");
+const ioClient = require("socket.io-client");
 
-//	 console.log("A New Client is connected");
-// });
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+
+
+let users = [];
+
+// css static
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+
+// Route the application
+app.get('/', (req, res) => {
+  let client = ioClient.connect("http://localhost:3000");
+  client.on('connection', socket => console.log('user connected'))
+
+  res.sendFile(path.join(__dirname + '/public/dashboard.html'));
+})
 
 // some rooms
-const gameRooms = ["Room-1", "Room-2", "Room-3"];
+// const gameRooms = ["Room-1", "Room-2", "Room-3"];
 
-io.of("/games").on("connection", socket => {
+// io.of("/games").on("connection", socket => {
 
-  // emit an event when connect to the namespace
-  socket.emit('games-joined', 'Welcome to  Game Area');
+//   // emit an event when connect to the namespace
+//   socket.emit('games-joined', 'Welcome to  Game Area');
 
-  socket.on("joinRoom", (room) => {
-    if (gameRooms.includes(room)) {
-      socket.join(room);
-      return socket.emit('success', 'You have joined: ' + room)
-    } else {
-      return socket.emit('err', 'No Room name ' + room)
-    }
+//   socket.on("joinRoom", (room) => {
+//     if (gameRooms.includes(room)) {
+//       socket.join(room);
+//       return socket.emit('success', 'You have joined: ' + room)
+//     } else {
+//       return socket.emit('err', 'No Room name ' + room)
+//     }
+//   })
+
+// });
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+  io.on('connection', socket => {
+    console.log('New USER');
+    // users.push(socket);
   })
-
-});
-
-
-// Server listening to port
-http.listen(port, () => {
-  console.log("Server is listening on localhost: " + port);
 });
