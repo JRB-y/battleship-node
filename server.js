2d82763f135c45420d96a342ac136d023ed79216
jrb <jrb.youssef@gmail.com>
2019-11-20 00:13:47 +0100

    Lunching game

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 
-// Array of users (socket)
-let users = [];
+// List of all sockets 
+let connections = [];
+let players = [];
 
-// number of players connected to the socket to start a game
-let playersOnGame = 2;
 
-server.listen(8000);
+server.listen(8000, () => {
+  console.log('Server Listening on port 8000');
+});
 
 // Routing
 app.get("/", (req, res) => {
@@ -23,29 +24,66 @@ app.get("/", (req, res) => {
 | Static files
 */
 app.use('/modules', express.static(__dirname + '/node_modules/socket.io-client/dist'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Listening to The socket
 io.sockets.on('connection', (socket) => {
 
-  users.push(socket);
-  console.log(`A new user: ${users.length} player(s) online`);
+  connections.push(socket);
 
-  socket.on('register-name', () => {
-    console.log(1);
-    socket.emit('players-online');
-    console.log(2);
+  console.log('A new user: %s user(s) online', connections.length);
+
+
+  // new User
+  socket.on('new-user', function (nickname) {
+
+    socket.nickname = nickname;
+    players.push([socket.id, nickname]);
+
+    io.sockets.emit('welcome', players);
+
+  })
+
+
+  socket.on('send-game-request', function (host) {
+
+    let guest = search(socket.id, players);
+
+    io.to(host[0]).emit('game-request', {
+      host,
+      guest
+    });
   });
 
+  socket.on('game-init', function (gamePlayers) {
+    let guest = gamePlayers.guest;
+    let host = gamePlayers.host;
+
+    io.to(guest[0]).emit('game-ready', {
+      host,
+      guest
+    });
+  })
 
   // On disconnect
   socket.on('disconnect', function () {
-    console.log('User Disconncted');
-    users.forEach(function (item, index) {
+
+    connections.forEach(function (item, index) {
       if (item.id == socket.id) {
-        users.splice(index, 1);
-        console.log(`User disconnected: ${users.length} player(s) online`);
+        connections.splice(index, 1);
+        console.log(`User disconnected: ${connections.length} player(s) online`);
+        return true;
+      }
+    })
+
+    players.forEach(function (item, index) {
+      if (item[1] == socket.nickname) {
+
+        io.sockets.emit('user-disconcted', item);
+
+        players.splice(index, 1);
         return true;
       }
     })
@@ -53,4 +91,27 @@ io.sockets.on('connection', (socket) => {
 
 })
 
+
+
+
+io.of('game-room').on('connection', (socket) => {
+  console.log('User is in Gaming Room');
+  if (players.length == 2) {
+    console.log('The Game will start');
+  } else {
+    console.log('We are waitting the other player');
+  }
+
+});
+
 // socket server
+
+
+// UTILITIES
+function search(id, myArray) {
+  for (var i = 0; i < myArray.length; i++) {
+    if (myArray[i][0] === id) {
+      return myArray[i];
+    }
+  }
+}
